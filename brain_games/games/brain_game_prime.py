87594d4ac52e05f84ce1265bfdcf10a03d9d7546
Petr Malafeev <StandBy545@yandex.ru>
2024-02-08 19:31:47 +0300

    Fix is_prime function

--- brain_games/games/brain_game_prime.py
+++ brain_games/games/brain_game_prime.py
@@ -14,6 +14,9 @@ RULES_TEXT = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(num):
+    if num < 1:
+        return False
+
     for i in range(2, round((num ** 0.5)) + 1):
         if num % i == 0:
             return False
