a1fabdcdabe97c9088c0e51a3587bf8e6620c59f
Petr Malafeev <StandBy545@yandex.ru>
2024-02-13 07:52:50 +0300

    Refactor brain-calc game
    brain_calc script now uses games_engine to execute logic instead of
    built in play_game function.

--- brain_games/games/brain_game_calc.py
+++ brain_games/games/brain_game_calc.py
@@ -3,12 +3,8 @@
 import operator
 import random
 
-import brain_games.games.common_games_operations as cgo
-from brain_games.games.games_settings import (
-    CORRECT_ANSWERS_TO_WIN,
-    MAX_NUMBER,
-    MIN_NUMBER,
-)
+from brain_games.games.common_games_operations import generate_question
+from brain_games.games.games_settings import MAX_NUMBER, MIN_NUMBER
 
 RULES_TEXT = 'What is the result of the expression?'
 
@@ -27,23 +23,10 @@ def get_correct_answer(num1, num2, operator):
     return str(operators[operator](num1, num2))
 
 
-def play_game():
-    user_name = cgo.get_username_and_welcome_user()
-    print(RULES_TEXT)
-
-    for _ in range(CORRECT_ANSWERS_TO_WIN):
-        number1 = random.randint(MIN_NUMBER, MAX_NUMBER)
-        number2 = random.randint(MIN_NUMBER, MAX_NUMBER)
-        operator = get_random_operator(operators)
-        print(cgo.get_question(number1, operator, number2))
-
-        answer = cgo.get_answer()
-        correct_answer = get_correct_answer(number1, number2, operator)
-
-        if not cgo.is_correct_answer(answer, correct_answer):
-            cgo.print_lose_game_text(answer, correct_answer, user_name)
-            break
-        print('Correct!')
-
-    else:
-        cgo.print_win_game_text(user_name)
+def generate_game_round():
+    number1 = random.randint(MIN_NUMBER, MAX_NUMBER)
+    number2 = random.randint(MIN_NUMBER, MAX_NUMBER)
+    operator = get_random_operator(operators)
+    question = generate_question(number1, operator, number2)
+    correct_answer = get_correct_answer(number1, number2, operator)
+    return question, correct_answer
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-"""
-This script starts brain-calc game
+"""This script starts brain-calc game.
 
 Brain-calc is a game, where player needs to solve arithmetic problems.
-
 """
 
-from brain_games.games.brain_game_calc import play_game
+from brain_games.games import brain_game_calc
+from brain_games.games.games_engine import games_engine
 
 
 def main():
-    play_game()
+    games_engine(brain_game_calc)
 
 
 if __name__ == '__main__':
