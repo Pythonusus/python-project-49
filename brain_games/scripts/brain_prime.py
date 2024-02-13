85fbdf48bb7bd7ba15e87f3d6ffb975cfbdbcd03
Petr Malafeev <StandBy545@yandex.ru>
2024-02-13 08:05:16 +0300

    Refactor brain-prime game
    brain_prime script now uses games_engine to execute logic instead of
    built in play_game function.

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-"""
-This script starts brain-prime game
+"""This script starts brain-prime game.
 
 Brain-prime is a game, where player needs to define if the given number
 is prime.
-
 """
 
-from brain_games.games.brain_game_prime import play_game
+from brain_games.games import brain_game_prime
+from brain_games.games.games_engine import games_engine
 
 
 def main():
-    play_game()
+    games_engine(brain_game_prime)
 
 
 if __name__ == '__main__':
