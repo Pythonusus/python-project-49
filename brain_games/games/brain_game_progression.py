338145b3572524dce65bf91d0da67b01a8b5f776
Petr Malafeev <StandBy545@yandex.ru>
2024-02-13 08:24:19 +0300

    Refactor brain-progression game
    brain_progression script now uses games_engine to execute logic instead of
    built in play_game function.

--- brain_games/games/brain_game_progression.py
+++ brain_games/games/brain_game_progression.py
@@ -2,8 +2,7 @@
 
 import random
 
-import brain_games.games.common_games_operations as cgo
-from brain_games.games.games_settings import CORRECT_ANSWERS_TO_WIN
+from brain_games.games.common_games_operations import generate_question
 
 RULES_TEXT = 'What number is missing in the progression?'
 MIN_PROGRESSION_START = 0
@@ -30,50 +29,42 @@ def get_progression_copy_with_hidden_element(progression, index):
     return new_progression
 
 
-def play_game():
-    user_name = cgo.get_username_and_welcome_user()
-    print(RULES_TEXT)
+def get_correct_answer(progression, hidden_element_index):
+    return str(progression[hidden_element_index])
 
-    for _ in range(CORRECT_ANSWERS_TO_WIN):
-        progression_start = random.randint(
-            MIN_PROGRESSION_START,
-            MAX_PROGRESSION_START
+
+def generate_game_round():
+    progression_start = random.randint(
+        MIN_PROGRESSION_START,
+        MAX_PROGRESSION_START
+    )
+
+    progression_common_difference = random.randint(
+        MIN_COMMON_DIFFERENCE,
+        MAX_COMMON_DIFFERENCE
+    )
+
+    progression_length = random.randint(
+        MIN_PROGRESSON_LENGTH,
+        MAX_PROGRESSON_LENGTH
+    )
+
+    progression = generate_progression(
+        progression_start,
+        progression_common_difference,
+        progression_length
+    )
+
+    hidden_element_index = random.randrange(len(progression))
+
+    progression_with_hidden_element = (
+        get_progression_copy_with_hidden_element(
+            progression,
+            hidden_element_index
         )
+    )
 
-        progression_common_difference = random.randint(
-            MIN_COMMON_DIFFERENCE,
-            MAX_COMMON_DIFFERENCE
-        )
+    question = generate_question(*progression_with_hidden_element)
+    correct_answer = get_correct_answer(progression, hidden_element_index)
 
-        progression_length = random.randint(
-            MIN_PROGRESSON_LENGTH,
-            MAX_PROGRESSON_LENGTH
-        )
-
-        progression = generate_progression(
-            progression_start,
-            progression_common_difference,
-            progression_length
-        )
-
-        hidden_element_index = random.randrange(len(progression))
-
-        progression_with_hidden_element = (
-            get_progression_copy_with_hidden_element(
-                progression,
-                hidden_element_index
-            )
-        )
-
-        print(cgo.get_question(*progression_with_hidden_element))
-
-        answer = cgo.get_answer()
-        correct_answer = str(progression[hidden_element_index])
-
-        if not cgo.is_correct_answer(answer, correct_answer):
-            cgo.print_lose_game_text(answer, correct_answer, user_name)
-            break
-        print('Correct!')
-
-    else:
-        cgo.print_win_game_text(user_name)
+    return question, correct_answer
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-"""
-This script starts brain-progression game
+"""This script starts brain-progression game.
 
 Brain-progression is a game, where player needs to find missing
 number in progression.
-
 """
 
-from brain_games.games.brain_game_progression import play_game
+from brain_games.games import brain_game_progression
+from brain_games.games.games_engine import games_engine
 
 
 def main():
-    play_game()
+    games_engine(brain_game_progression)
 
 
 if __name__ == '__main__':
