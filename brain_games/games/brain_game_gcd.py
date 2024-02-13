4d2466d10c67f8acc88484fb93f2fc7572bb80cf
Petr Malafeev <StandBy545@yandex.ru>
2024-02-13 08:01:33 +0300

    Refactor brain-gcd game
    brain_gcd script now uses games_engine to execute logic instead of
    built in play_game function.

--- brain_games/games/brain_game_gcd.py
+++ brain_games/games/brain_game_gcd.py
@@ -3,12 +3,8 @@
 import math
 import random
 
-import brain_games.games.common_games_operations as cgo
-from brain_games.games.games_settings import (
-    CORRECT_ANSWERS_TO_WIN,
-    MAX_NUMBER,
-    MIN_NUMBER,
-)
+from brain_games.games.common_games_operations import generate_question
+from brain_games.games.games_settings import MAX_NUMBER, MIN_NUMBER
 
 RULES_TEXT = 'Find the greatest common divisor of given numbers.'
 
@@ -17,22 +13,9 @@ def get_correct_answer(num1, num2):
     return str(math.gcd(num1, num2))
 
 
-def play_game():
-    user_name = cgo.get_username_and_welcome_user()
-    print(RULES_TEXT)
-
-    for _ in range(CORRECT_ANSWERS_TO_WIN):
-        number1 = random.randint(MIN_NUMBER, MAX_NUMBER)
-        number2 = random.randint(MIN_NUMBER, MAX_NUMBER)
-        print(cgo.get_question(number1, number2))
-
-        answer = cgo.get_answer()
-        correct_answer = get_correct_answer(number1, number2)
-
-        if not cgo.is_correct_answer(answer, correct_answer):
-            cgo.print_lose_game_text(answer, correct_answer, user_name)
-            break
-        print('Correct!')
-
-    else:
-        cgo.print_win_game_text(user_name)
+def generate_game_round():
+    number1 = random.randint(MIN_NUMBER, MAX_NUMBER)
+    number2 = random.randint(MIN_NUMBER, MAX_NUMBER)
+    question = generate_question(number1, number2)
+    correct_answer = get_correct_answer(number1, number2)
+    return question, correct_answer
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-"""
-This script starts brain-gcd game
+"""This script starts brain-gcd game.
 
-Brain-gcd is a game, where player needs to find greatest common devisor
-
+Brain-gcd is a game, where player needs to find greatest common devisor.
 """
 
-from brain_games.games.brain_game_gcd import play_game
+from brain_games.games import brain_game_gcd
+from brain_games.games.games_engine import games_engine
 
 
 def main():
-    play_game()
+    games_engine(brain_game_gcd)
 
 
 if __name__ == '__main__':
