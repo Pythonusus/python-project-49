80fbb2858a33b510342a7a05d1330ecf731bcf90
Petr Malafeev <StandBy545@yandex.ru>
2024-02-20 06:56:35 +0300

    Refactor brain_game_gcd.py

--- brain_games/games/brain_game_gcd.py
+++ brain_games/games/brain_game_gcd.py
@@ -9,13 +9,9 @@ from brain_games.games.common_games_operations import generate_question
 RULES_TEXT = 'Find the greatest common divisor of given numbers.'
 
 
-def get_correct_answer(num1, num2):
-    return str(math.gcd(num1, num2))
-
-
 def generate_game_round():
     number1 = random.randint(MIN_NUMBER, MAX_NUMBER)
     number2 = random.randint(MIN_NUMBER, MAX_NUMBER)
     question = generate_question(number1, number2)
-    correct_answer = get_correct_answer(number1, number2)
+    correct_answer = str(math.gcd(number1, number2))
     return question, correct_answer
